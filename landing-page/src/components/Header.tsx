6649e2595fd72f26fff96078326b8f816ea9e5b8
Paula Singh <paulasofiasingh@gmail.com>
2025-05-22 17:39:45 -0300

    Agregue el logo

--- landing-page/src/components/Header.tsx
+++ landing-page/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from './styles/Header.module.css';
 
 export default function Header() {
@@ -5,6 +6,11 @@ export default function Header() {
     <header className={styles.header}>
       <nav>
         <ul className={styles.navList}>
+          <li className={styles.logo}>
+            <a href="#inicio">
+              <Image src="/logo.jpg" alt="Logo" width={100} height={79} />
+            </a>
+          </li>
           <li className={styles.navItem}><a href="#inicio" className={styles.navLink}>Inicio</a></li>
           <li className={styles.navItem}><a href="#planes">Planes</a></li>
           <li className={styles.navItem}><a href="#testimonios">Testimonios</a></li>
