07a4fd0001f05e66c300471be7c51569028779d2
Paula Singh <paulasofiasingh@gmail.com>
2025-06-09 11:31:56 -0300

    Se hizo responsive la seccion Sobre mi

--- landing-page/src/components/AboutMeSection.jsx
+++ landing-page/src/components/AboutMeSection.jsx
@@ -5,12 +5,11 @@ export default function AboutMeSection() {
   return (
     <section id="sobremi" className={styles.aboutSection}>
       <div className={styles.imageWrapper}>
-        <img src="sobremi.jpeg" alt="Sofía Sanz" className={styles.image} />
+        <img src="/sobremi.jpeg" alt="Sofía Sanz" className={styles.image} />
       </div>
 
       <div className={styles.textWrapper}>
         <h2 className={styles.title}>Sobre mí</h2>
-
         <p className={styles.description}>
           Soy atleta y entrenadora hace más de 10 años. Estudié en las mejores
           academias de Buenos Aires y me preparé a nivel competitivo con los
